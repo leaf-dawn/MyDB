d430955faa9ca8aee8b120f09471cb701aef7c87
fzw-code <81961566+fzw-code@users.noreply.github.com>
2022-12-06 19:18:04 +0800

    feat(index_manage):添加创建红黑树

--- backend/index_manage/tree.go
+++ backend/index_manage/tree.go
@@ -2,6 +2,7 @@ package index_manage
 
 import (
 	dm "briefDb/backend/data_manage"
+	tm "briefDb/backend/transaction_manage"
 	"briefDb/backend/utils"
 	"sync"
 )
@@ -22,4 +23,23 @@ type bPlusTree struct {
 	bootLock     sync.Mutex  //锁根节点的
 
 	DataManager dm.DataManager //存储该树的文件的datamanager
+}
+
+// Create
+// 创建一棵B+树, 并返回其bootUUID.
+func Create(dm dm.DataManager) (utils.UUID, error) {
+	// 创建以空根节点
+	rawRoot := newNilRootRaw()
+	// 添加到文件中
+	rootUUID, err := dm.Insert(tm.SUPER_TRANSACTION_ID, rawRoot)
+	if err != nil {
+		return utils.NilUUID, err
+	}
+	// 根节点uuid保存到文件，并返回存储根节点uuid的dataitem的uuid
+	bootUUID, err := dm.Insert(tm.SUPER_TRANSACTION_ID, utils.UUIDToRaw(rootUUID))
+	if err != nil {
+		return utils.NilUUID, err
+	}
+
+	return bootUUID, nil
 }
